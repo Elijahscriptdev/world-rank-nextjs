aa628a09b43b08222c13fe15fa72252e839d97f8
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-19 18:45:01 +0100

    filter by country name from search bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import CountriesTable from "../component/CountriesTable/CountriesTable";
 import Layout from "../component/Layout/Layout";
 import SearchInput from "../component/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  // filter by country name
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
   console.log(countries);
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder='Filter by Name, Region or Sub-region' />
+      <SearchInput
+        placeholder='Filter by Name, Region or Sub-region'
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
